63f44e38983e05d06453dcae6c0ce2ece6182d7e
Ian Plumpton <cellscavengergames@gmail.com>
2020-08-27 14:38:23 +0100

    Gravity update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,12 @@ public class Player : MonoBehaviour
     private CharacterController _controller;
 
     [SerializeField]
-    private float _playerSpeed = 5.0f;
+    private float _playerSpeed = 8.0f;
+    [SerializeField]
+    private float _gravity = 1.0f;
+    [SerializeField]
+    private float _jumpHeight = 23.0f;
+    private float _yVelocity;
 
     void Start()
     {
@@ -18,9 +23,22 @@ public class Player : MonoBehaviour
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-
         Vector3 direction = new Vector3(horizontalInput, 0, 0);
         Vector3 velocity = direction * _playerSpeed;
+
+        if (_controller.isGrounded == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+            }
+        }
+        else
+        {
+            _yVelocity -= _gravity;
+        }
+
+        velocity.y = _yVelocity;
 
         _controller.Move(velocity *  Time.deltaTime);
     }
